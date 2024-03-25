a6495e010fa16b7a5f005dbfa93bb33d3315fd62
Lenny Wandeto <lenny.wandeto@student.moringaschool.com>
2024-03-25 15:05:52 +0300

    changes index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(location) {
+    let blocks = Math.abs(location - 42);
+    return blocks
+}
+
+function distanceFromHqInFeet(location) {
+    let feet = Math.abs((location - 42)) * 264
+    return feet
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let feet = Math.abs((destination - start)) * 264
+    return feet
+}
+
+function calculatesFarePrice(start, destination) {
+    let feet = Math.abs((destination - start) * 264)
+    if (feet < 400) {
+        let fare = 0;
+        return fare;
+    } else if (feet >= 400 && feet < 2000) {
+        let feetNew = feet - 400;
+        let 
+        fare = feetNew * 0.02;
+        return fare;
+
+    } else if (feet > 2500) {
+        return "cannot travel that far"
+    } else {
+        return 25
+    }
+}
